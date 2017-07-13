1b386057a5576d427c750622e478d98481902161
Mariano d'almeida <mdalmeida@gmail.com>
2017-07-13 16:43:51 -0300

    Ahora se puede ver los archivos de un directorio

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-app.use(express.static(path.join(__dirname, 'dist')));
+var serveIndex = require('serve-index')
+var serveStatic = require('serve-static')
+
+// Serve directory indexes for public/ftp folder (with icons)
+app.use(serveIndex('dist'));
+// Serve up public/ftp folder files
+app.use(serveStatic('dist'));
+
+// app.use(express.static(path.join(__dirname, 'dist')));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
